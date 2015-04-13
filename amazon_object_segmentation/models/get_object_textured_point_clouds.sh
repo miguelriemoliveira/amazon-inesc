c82e7a3949f29145994891927ebb43df583da505
Miguel Riem de Oliveira <m.riem.oliveira@gmail.com>
2015-04-13 10:55:12 +0100

    mike

--- amazon_object_segmentation/models/get_object_textured_point_clouds.sh
+++ amazon_object_segmentation/models/get_object_textured_point_clouds.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 echo "This will download the amazon objects point clouds. You need 1.7GB for this."
-wget https://www.dropbox.com/s/zeq5k82wj6exusw/amazon_object_textured_point_clouds.tar.gz?dl=0
-tar -xvf amazon_object_textured_point_clouds.tar.gz?dl=0
-rm -i amazon_object_textured_point_clouds.tar.gz?dl=0
 
-echo "Point clouds of objects installed. Check ./amazon_object_textured_point_clouds"
+wget https://www.dropbox.com/s/b74co6v785scj0r/amazon_object_textured_meshes.tar.gz?dl=0
+tar -xvf amazon_object_textured_meshes.tar.gz?dl=0
+rm -i amazon_object_textured_meshes.tar.gz?dl=0
+
+echo "Point clouds of objects installed. Check ./amazon_object_textured_meshes"
